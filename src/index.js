cf74e60e7660341b2d56dca11eecb9bda11ab4bb
git-study <55270985+ly990412@users.noreply.github.com>
2022-11-18 12:59:55 -0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Hard
+ from './Hard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Hard />
+  </React.StrictMode>
+);
+
